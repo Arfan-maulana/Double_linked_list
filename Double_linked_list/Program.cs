e0d1eb0ddfff89f63744e2f39f3087a2c8c72b4a
ASUS <ASUS@LAPTOP-M7MD7ENC>
2022-11-14 16:47:34 +0700

    Add project files.

---
+++ Double_linked_list/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Double_linked_list
+{
+    internal class Program
+    {
+        class Node
+        {
+
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}

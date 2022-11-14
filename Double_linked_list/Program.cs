2db81c8c5177960efb0d32437e3fa2a9430f8528
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-11-14 19:22:21 +0700

    membut switch case

--- Double_linked_list/Program.cs
+++ Double_linked_list/Program.cs
@@ -65,12 +65,12 @@ namespace Double_linked_list
                         return;
                     }
 
-                    
+
                 }
                 newNode.next = current;
                 newNode.prev = previous;
 
-                if(current == null)
+                if (current == null)
                 {
                     newNode.next = current;
                     previous.next = newNode;
@@ -82,19 +82,19 @@ namespace Double_linked_list
             public bool search(int rollNo, ref Node previous, ref Node current)
             {
 
-                for(previous = current =START; current != null &&
+                for (previous = current = START; current != null &&
                     rollNo != current.noMhs; previous = current,
                     current = current.next) { }
                 return (current != null);
-                    
+
             }
             public bool dellNode(int rollNo)
             {
                 Node previous, current;
                 previous = current = null;
-                if (search(rollNo, ref previous, ref current)== false)
+                if (search(rollNo, ref previous, ref current) == false)
                     return false;
-                if(current.next == null)
+                if (current.next == null)
                 {
                     previous.next = null;
                     return true;
@@ -107,7 +107,7 @@ namespace Double_linked_list
                         START.prev = null;
                     return true;
                 }
-                
+
                 previous.next = current.next;
                 current.next.prev = previous;
                 return true;
@@ -127,15 +127,128 @@ namespace Double_linked_list
                 else
                 {
                     Console.WriteLine("\nRecord in the ascending order of " + " roll number are :\n");
-                    
+                    Node currentNode;
+                    for (currentNode = START; currentNode != null; currentNode = currentNode.next)
+                    { }
+
+                    while (currentNode != null)
+                    {
+                        Console.Write(currentNode.noMhs + "" + currentNode.name + "\n");
+                        currentNode = currentNode.prev;
+                    }
+
 
                 }
             }
-            static void Main(string[] args)
+            public void descending()
             {
+                if (listEmpty())
+                    Console.WriteLine("\nList is Empty");
+                else
+                {
+                    Console.WriteLine("\nRecord in the ascending order of" + "roll number are:\n");
+                    Node currentNode;
+                    for (currentNode = START; currentNode != null; currentNode = currentNode.next) { }
 
+                    while (currentNode != null)
+                    {
+                        Console.Write(currentNode.noMhs + "" + currentNode.name + "\n");
+                        currentNode = currentNode.prev;
+                    }
+
+                }
             }
+            class program
+            {
+                static void Main(string[] args)
+                {
+                    DoubleLinkedList obj = new DoubleLinkedList();
+                    while (true)
+                    {
+                        Console.WriteLine("\nMenu");
+                        Console.WriteLine("1. Add a record to the list ");
+                        Console.WriteLine("2. Deleted a record from the list ");
+                        Console.WriteLine("3. View all record in the ascending order of roll numbers ");
+                        Console.WriteLine("4. View all record in the descending order of roll numbers ");
+                        Console.WriteLine("5. Search for a record in the list ");
+                        Console.WriteLine("6. Exit\n");
+                        Console.WriteLine("Enter your choice (1-6): ");
+                        char ch = Convert.ToChar(Console.ReadLine());
+                        switch (ch)
+                        {
 
+                            case '1':
+                                {
+                                    obj.addnote();
+                                }
+                                break;
+                            case '2':
+                                {
+                                    if (obj.listEmpty())
+                                    {
+                                        Console.WriteLine("\nList is empty");
+                                        break;
+                                    }
+                                    Console.Write("\nenter the roll number of the student" + "whose record is to be deleted : ");
+                                    int rolNo = Convert.ToInt32(Console.ReadLine());
+                                    Console.WriteLine();
+                                    if (obj.dellNode(rolNo) == false)
+                                        Console.WriteLine("Record not found");
+                                    else
+                                        Console.WriteLine("record with roll number " + rolNo + "deleted \n");
+
+
+                                }
+                                break;
+                            case '3':
+                                {
+                                    obj.ascending();
+                                }
+                                break;
+                            case '4':
+                                {
+                                    obj.descending();
+                                }
+                                break;
+                            case '5':
+                                {
+                                    if (obj.listEmpty() == true)
+                                    {
+                                        Console.WriteLine("\nList is empty");
+                                        break;
+                                    }
+                                    Node prev, curr;
+                                    prev = curr = null;
+                                    Console.Write("\nentr the roll number of the student whore record you want to search: ");
+                                    int num = Convert.ToInt32(Console.ReadLine());
+                                    if (obj.search(num, ref prev, ref curr) == false)
+                                        Console.WriteLine("\n Record not found");
+                                    else
+                                    {
+                                        Console.WriteLine("\n Record found");
+                                        Console.WriteLine("\n Roll number : " + curr.noMhs);
+                                        Console.WriteLine("\n Namw :" + curr.name);
+                                    }
+                                }
+                                break;
+                            case '6':
+                                return;
+                            default:
+                                {
+                                    Console.WriteLine("\nInvalid option");
+                                }
+                                break;
+
+
+                        }
+                    
+                    }
+                     catch (Exception e)
+                    {
+                        Console.WriteLine("Check for the valueas entered");
+                    }
+                }
+            }
 
         }
     }

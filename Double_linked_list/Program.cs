dea59f3ade1705117b064b5a0f086faf35a0736e
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-11-14 17:05:46 +0700

    menambahkankan function if

--- Double_linked_list/Program.cs
+++ Double_linked_list/Program.cs
@@ -34,9 +34,19 @@ namespace Double_linked_list
             {
                 int nim;
                 string nm;
-                Console.WriteLine("/n Enter the roll number off the student :");
+                Console.WriteLine("\n Enter the roll number off the student :");
                 nim = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n Enter name off the student :");
+                nm = Console.ReadLine();
+                Node newNode = new Node();
+                newNode.noMhs = nim;
+                newNode.name = nm;
 
+                if((START != null) && (nim == START.noMhs))
+                {
+                    Console.WriteLine("\n Duplicate number not allowed ");
+                    return;
+                }
 
 
 
